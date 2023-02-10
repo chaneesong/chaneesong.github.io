7f9cdddaeb022126813e914eae686fd362d17679
sch <sch102403@gmail.com>
2023-02-10 17:09:38 +0900

    blog update

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230210.155909';
+const cacheName = 'chirpy-20230210.160917';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
