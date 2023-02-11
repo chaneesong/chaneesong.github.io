a6b0ff51614167087b4b781fd1a42fe2737885b6
sch <sch102403@gmail.com>
2023-02-11 20:51:25 +0900

    Revert "사이드바 홈 버튼 수정"
    This reverts commit e299e5b8a006a73b45c96d9f3cee7924c42117a3.

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230211.203833';
+const cacheName = 'chirpy-20230211.145046';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
