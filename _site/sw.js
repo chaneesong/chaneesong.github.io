a04f62af983bbfffc070cb02fa941a5e46bf2536
sch <sch102403@gmail.com>
2023-02-26 17:19:56 +0900

    revert ad script

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230216.200444';
+const cacheName = 'chirpy-20230226.171600';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
